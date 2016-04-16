935d1fdb26eb746dd0752cd81a23857e0eb82214
JoJihun <kkkdkkkd@naver.com>
2016-04-16 21:36:28 +0900

    여태 자료 다시올림/ 다른컴퓨터에서 안돌아갓음

---
+++ srpg/Assets/Scripts/Battle/Map/Hex.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Point
+{
+    int X;
+    float Y;
+    int Z;
+    public void SetY(float y)
+    {
+        Y = y;
+    }
+    public int GetX()
+    {
+        return X;
+    }
+    public int GetY()
+    {
+        return (int)Y;
+    }
+    public int GetZ()
+    {
+        return Z;
+    }
+    public Point(int x, float y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+    public override string ToString()
+    {
+        return "["+X+" "+Y+" "+Z+"]";
+    }
+    public static Point operator +(Point p1,Point p2)
+    {
+       return new Point(p1.GetX() + p2.GetX(), p1.GetY() + p2.GetY(), p1.GetZ() + p2.GetZ());
+    }
+    public static bool operator ==(Point p1,Point p2)
+    {
+        return (p1.GetX() == p2.GetX() && p1.GetY() == p2.GetY() && p1.GetZ() == p2.GetZ());
+    }
+    public static bool operator !=(Point p1, Point p2)
+    {
+        return (p1.GetX() != p2.GetX() && p1.GetY() != p2.GetY() && p1.GetZ() != p2.GetZ());
+    }
+    public static Point operator -(Point p1, Point p2)
+    {
+        return new Point(p1.GetX() - p2.GetX(), p1.GetY() - p2.GetY(), p1.GetZ() - p2.GetZ());
+    }
+
+}
+
+public class Hex : MonoBehaviour {
+    public Point MapPos;
+    public int matid;
+    public bool Passable = true;
+    public Material mat1;
+    public Material mat2;
+    public Material mat3;
+    public Color mat_color = Color.white;
+	// Use this for initialization
+    void Start()
+    {
+        if (matid == 1)
+        {
+            GetComponent<Renderer>().material = mat1;
+            Passable = true;
+        }
+        if (matid == 2)
+        {
+            GetComponent<Renderer>().material = mat2;
+            Passable = false;
+        }
+        if (matid == 3)
+        {
+            GetComponent<Renderer>().material = mat3;
+            Passable = false;
+        }
+       
+    }
+    
+	// Update is called once per frame
+	void Update () {
+    }
+ 
+    public void SetMapPos(Point pos)
+    {
+        pos.SetY((float)1);
+        MapPos = pos;
+        
+    }
+
+    public void SetMat(int id)
+    {
+        matid = id;
+    }
+
+    public void SetMapPos(int x,float y,int z)
+    {
+        MapPos = new Point(x, y, z);
+    }
+    void OnMouseDown()
+    {
+        
+        PlayerManager pm = PlayerManager.GetInst();
+        PlayerBase pb = pm.Players[pm.CurTurnIdx];
+        Debug.Log(MapPos + "OnMouseDown");
+        if (pb.act == ACT.SUMMONES)
+        {
+            PlayerManager.GetInst().GenPlayer(MapPos.GetX(),MapPos.GetZ());
+        }
+        if (pb.act==ACT.IDLE)
+        {
+            if(Passable==true)
+            {
+                GetComponent<Renderer>().material.color = Color.blue;
+                mat_color = Color.blue;
+            }
+            else
+            {
+                GetComponent<Renderer>().material.color = Color.white;
+                mat_color = Color.white;
+            }
+            Passable = !Passable;
+
+        }
+        else if(pb.act==ACT.MOVEHILIGHT)
+        {
+             pm.MovePlayer(pm.Players[pm.CurTurnIdx].CurHex, this);
+        
+       }
+       
+        
+    }
+}
---
+++ srpg/Assets/Scripts/Battle/Player/PlayerManager.cs
@@ -0,0 +1,186 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class PlayerManager : MonoBehaviour {
+    private static PlayerManager inst = null;
+    public int parentidx = 0;
+    public GameObject GO_player;
+    public GameObject GO_aiplayer;
+
+    public int Monster_num = 0;
+    public List<PlayerBase> Players = new List<PlayerBase>();
+    public int CurTurnIdx = 0;
+    public float m_y;
+    private float turnOverTiem;
+    private float curTurnOverTiem;
+
+   
+
+    public void SetTurnOverTime(float time)
+    {
+        turnOverTiem = time;
+        curTurnOverTiem = Time.smoothDeltaTime;
+    }
+    public static PlayerManager GetInst()
+    {
+        return inst;
+    }
+    void Awake()
+    {
+        turnOverTiem = 0f;
+        curTurnOverTiem = 0f;
+        inst = this;
+    }
+	// Use this for initialization
+	void Start () {
+
+	}
+	void CheckTurnOver()
+    {
+        if(curTurnOverTiem!=0)
+        {
+            curTurnOverTiem += Time.deltaTime;
+            if(curTurnOverTiem>=turnOverTiem)
+            {
+                curTurnOverTiem = 0;
+                TurnOver();
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        CheckTurnOver();
+	}
+    public void HilightSummons()
+    {
+        MapManager.GetInst().HilightMoveRange(Players[CurTurnIdx].CurHex, 3);
+        Players[CurTurnIdx].act = ACT.SUMMONES;
+    }
+    public void GenPlayer(int x,int z)
+    {
+        UserPlayer userplayer = ((GameObject)Instantiate(GO_player)).GetComponent<UserPlayer>();
+        Hex hex = MapManager.GetInst().GetPlayerHex(x, 0, z);
+        userplayer.CurHex = hex;
+        Vector3 v = userplayer.CurHex.transform.position;
+        v.y = 1.0f;
+        userplayer.transform.position = v;
+        Players.Add(userplayer);
+        MapManager.GetInst().ResetMapColor();
+    }
+    public void GenPlayerTest()
+    {
+        UserPlayer userplayer = ((GameObject)Instantiate(GO_player)).GetComponent<UserPlayer>();
+        Hex hex = MapManager.GetInst().GetPlayerHex(0, 0, 0);
+        userplayer.CurHex = hex;
+        Vector3 v = userplayer.CurHex.transform.position;
+        v.y = 1.0f;
+        userplayer.transform.position = v;
+        Players.Add(userplayer);
+
+        for (int i = 0; i < Monster_num; ++i)
+        {
+            AIPlayer aiplayer = ((GameObject)Instantiate(GO_aiplayer)).GetComponent<AIPlayer>();
+            hex = MapManager.GetInst().GetPlayerHex(1, 0, 8-i);
+            aiplayer.CurHex = hex;
+            aiplayer.CurHex.Passable = false;
+            Vector3 p = aiplayer.CurHex.transform.position;
+            p.y = m_y;
+            aiplayer.transform.position = p;
+            Players.Add(aiplayer);
+        }
+    }
+    public void MovePlayer(Hex start,Hex dest)
+    {
+        PlayerBase pb = Players[CurTurnIdx];
+        if (MapManager.GetInst().IsReachAble(start, dest, pb.status.MoveRange) == false)
+        {
+            return; 
+        }
+        if (pb.act == ACT.MOVEHILIGHT)
+        {
+            float distance = MapManager.GetInst().GetDistance(start, dest);
+            if (distance <= Players[CurTurnIdx].status.MoveRange && distance != 0 && dest.Passable == true)
+            {
+                pb.MoveHexes = MapManager.GetInst().GetPath(start, dest);
+                if (pb.MoveHexes.Count == 0)
+                    return;
+                pb.act = ACT.MOVING;
+
+                MapManager.GetInst().ResetMapColor();
+            }
+        }
+  
+    }
+    public void TurnOver()
+    {
+        MapManager.GetInst().ResetMapColor(); 
+        PlayerBase pb = Players[CurTurnIdx];
+        pb.act = ACT.IDLE;
+        if (Players.Count > 0)
+            CurTurnIdx++;
+        if (CurTurnIdx >= Players.Count)
+        {         
+               CurTurnIdx = 0;
+        }
+        Manager.GetInst().MoveCamPosToTile(Players[CurTurnIdx].CurHex);
+      
+    }
+
+    public void RemovePlayer(PlayerBase pb)
+    {
+        
+        if (pb.main_char == false)
+        {
+            int cnt = 0;
+            for (int i = 0; i < Players.Count; i++)
+            {
+                if (pb.live == false)
+                {
+                        cnt++;
+                 }
+            }
+            Players.Remove(pb);
+            GameObject.Destroy(pb.gameObject);
+            if(cnt+1>=Players.Count)
+                SceneManager.LoadScene(2);
+
+        }
+        else
+        {
+            GameObject.Destroy(pb.gameObject);
+            SceneManager.LoadScene(0);
+        }
+    
+        
+    }
+    public void MouseInputProc(int btn)
+    {
+        if(btn==1)
+        {
+            //step - aI일때는 리턴
+        
+            PlayerBase pb = Players[CurTurnIdx];
+            if(pb is AIPlayer)
+            {
+                return;
+            }
+            //step1 idle 할일 x
+            ACT act = Players[CurTurnIdx].act;
+            if(act==ACT.IDLE)
+            {
+                MapManager.GetInst().ResetMapColor();
+                return;
+            }
+            //step2 attack 무브일떄 하이라이트 초기화 
+            if (act == ACT.MOVEHILIGHT||act==ACT.ATTACKHIGHLIGHT)
+            {
+                MapManager.GetInst().ResetMapColor();
+                Players[CurTurnIdx].act = ACT.IDLE;
+                
+                return;
+            }
+        }
+    }
+
+}
