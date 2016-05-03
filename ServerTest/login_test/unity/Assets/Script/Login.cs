e05b0eb8937a2610429c35fca2ad049284cd89d1
lee Jinsoo <tmf2676@gmail.com>
2016-05-03 17:58:13 +0900

    <유니티 < - > php 로그인 시스템 재커밋 >

---
+++ ServerTest/login_test/unity/Assets/Script/Login.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Login : MonoBehaviour {
+    InputField id;
+    InputField pass;
+	// Use this for initialization
+    public void setId(InputField id)
+    {
+        this.id = id;
+    }
+    public void setPass(InputField pass)
+    {
+        this.pass = pass;
+    }
+	public void loginphp()
+    {
+        StartCoroutine(Printphp());
+    }
+	
+	// Update is called once per frame
+    IEnumerator Printphp()
+    {
+        string url = "http://localhost/testconnectCsharp.php";
+        WWWForm form = new WWWForm();
+        form.AddField("ID", id.text);
+        form.AddField("PASSWD", pass.text);
+
+        WWW www = new WWW(url, form);
+
+        yield return www;
+
+        if(null == www.error)
+        {
+            Debug.Log(www.text);
+
+        }
+        else
+        {
+            Debug.Log(www.error);
+
+        }
+    }
+}
