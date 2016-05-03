e12ef0428c35532e71350c387931cb066779fb53
lee Jinsoo <tmf2676@gmail.com>
2016-05-03 17:42:58 +0900

    <로그인 연동 시스템 완성 // 세부적인 크기 조절 필요함 >

--- ServerTest/login_test/unity/Script/Login.cs
+++ ServerTest/login_test/unity/Script/Login.cs
@@ -1,20 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Login : MonoBehaviour {
-
+    InputField id;
+    InputField pass;
 	// Use this for initialization
-	void Start () {
+    public void setId(InputField id)
+    {
+        this.id = id;
+    }
+    public void setPass(InputField pass)
+    {
+        this.pass = pass;
+    }
+	public void loginphp()
+    {
         StartCoroutine(Printphp());
-	}
+    }
 	
 	// Update is called once per frame
     IEnumerator Printphp()
     {
         string url = "http://localhost/testconnectCsharp.php";
         WWWForm form = new WWWForm();
-        form.AddField("ID", "jojihoon");
-        form.AddField("PASSWD", 7654321);
+        form.AddField("ID", id.text);
+        form.AddField("PASSWD", pass.text);
 
         WWW www = new WWW(url, form);
 
