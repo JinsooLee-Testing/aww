cef187a0e03e3bfc41e1c186e9b75f47c8257f1a
lee Jinsoo <tmf2676@gmail.com>
2016-05-09 19:56:59 +0900

    <Unity <-> PHP 통신 // DB 정보를 공유할 static 스크립트 제작 >

---
+++ ServerTest/login_test/unity/Assets/Script/StaticData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class StaticData {
+    public static string userId;
+    public static string userPasswd;
+    public static int userStage;
+
+
+ //   public void setUserId(string inputId) { userId = inputId; }
+ //   public string getUserId() { return userId; }
+ //   public void setUserPasswd(string inputPasswd) { userPasswd = inputPasswd; }
+ //   public string getUserPasswd() { return userPasswd; }
+ //   public void setUserStage(int inputStage) { userStage = inputStage; }
+ //   public int getUserStage() { return userStage; }
+ //   // Use this for initialization
+ //   void Start () {
+	
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
