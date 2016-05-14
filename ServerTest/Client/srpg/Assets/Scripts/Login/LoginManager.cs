9c8230b0bc26af9c74d18f7a29695e8e411214d9
lee Jinsoo <tmf2676@gmail.com>
2016-05-14 15:33:43 +0900

    <Client 복제 < - > PHP 로그인 연동 성공 >

---
+++ ServerTest/Client/srpg/Assets/Scripts/Login/LoginManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class LoginManager : MonoBehaviour
+{
+    InputField id;
+    InputField password;
+    // Use this for initialization
+    public void SetID(InputField id)
+    {
+        this.id = id;
+    }
+    
+    public void SetPassword(InputField password)
+    {
+        this.password = password;
+    }
+
+    public void LoginPhp()
+    {
+        StartCoroutine(Printphp());
+    }
+
+    IEnumerator Printphp()
+    {
+        string url = "http://localhost/Aww/AwwLogin.php";
+        //string url = "http://localhost/testconnectCsharp.php";
+        // string url = "http://localhost/Aww/login.php";
+        WWWForm form = new WWWForm();
+        form.AddField("ID", id.text);
+        form.AddField("PASSWD", password.text);
+
+        WWW www = new WWW(url, form);
+
+        yield return www;
+
+
+        if (null == www.error)
+        {
+            Debug.Log(www.text);
+            //StaticData.userId = id.text;
+            //StaticData.userPasswd = pass.text;
+            //setUserId(id.text);
+            //setUserPasswd(pass.text);
+            Application.LoadLevel("MainTitle");
+            //if ("0" == www.text)
+            //{
+            //    //StaticData.userStage = 0;
+            //    //Application.LoadLevel("main_scene");
+            //}
+            //else if ("1" == www.text)
+            //{
+            //    //StaticData.userStage = 1;
+            //    //Application.LoadLevel("main_scene");
+            //}
+            //else if ("2" == www.text)
+            //{
+            //    //StaticData.userStage = 2;
+            //    //Application.LoadLevel("main_scene");
+            //}
+            //if ("3" == www.text)
+            //{
+            //    //StaticData.userStage = 3;
+            //    Application.LoadLevel("main_scene");
+            //}
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+
+    }
+
+
+}
---
+++ ServerTest/login_test/unity/Assets/Script/Login.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Login : MonoBehaviour {
+    InputField id;
+    InputField pass;
+    
+    public void setId(InputField id)
+    {
+        this.id = id;
+    }
+    public void setPass(InputField pass)
+    {
+       
+        this.pass = pass;
+        
+    }
+	public void loginphp()
+    {
+        StartCoroutine(Printphp());
+    }
+	
+	// Update is called once per frame
+    IEnumerator Printphp()
+    {
+
+        string url = "http://localhost/testconnectCsharp.php";
+     // string url = "http://localhost/Aww/login.php";
+        WWWForm form = new WWWForm();
+        form.AddField("ID", id.text);
+        form.AddField("PASSWD", pass.text);
+
+        WWW www = new WWW(url, form);
+
+        yield return www;
+
+        
+        if(null == www.error)
+        {
+            StaticData.userId = id.text;
+            StaticData.userPasswd = pass.text;
+            //setUserId(id.text);
+            //setUserPasswd(pass.text);
+            //Application.LoadLevel("main_scene");
+            if ("0" == www.text)
+            {
+                StaticData.userStage = 0;
+                Application.LoadLevel("main_scene");
+            }
+            else if ("1" == www.text)
+            {
+                StaticData.userStage = 1;
+                Application.LoadLevel("main_scene");
+            }
+            else if ("2" == www.text)
+            {
+                StaticData.userStage = 2;
+                Application.LoadLevel("main_scene");
+            }
+            if ("3" == www.text)
+            {
+                StaticData.userStage = 3;
+                Application.LoadLevel("main_scene");
+            }
+        }
+        else
+        {
+            Debug.Log(www.error);     
+        }
+       
+    }
+}
