f0897ba2bf51497434de36f19f9a77a8d88cda3a
JoJihun <kkkdkkkd@naver.com>
2016-05-14 22:46:55 +0900

    스테이지 클리어내역 저장/ 저장한것을 표시하는 ui

--- srpg/Assets/Scripts/MainTitle/BattleStart.cs
+++ srpg/Assets/Scripts/MainTitle/BattleStart.cs
@@ -3,18 +3,34 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 public class BattleStart : MonoBehaviour {
     public int nextscene_num;
+    public bool stage_lock = false;
+    public Sprite rock;
+    public Sprite unrock;
+    public int stagenum;
 	// Use this for initialization
+    void Awake()
+    {
+      
+    }
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (StageManager.Getinst().cur_stage >= stagenum)
+            stage_lock = true;
+        if (stage_lock == false)
+            GetComponent<SpriteRenderer>().sprite = rock;
+        else
+            GetComponent<SpriteRenderer>().sprite = unrock;
+    }
     void OnMouseDown()
     {
-        SoundManager.GetInst().PlayClickSound();
-        SceneManager.LoadScene(nextscene_num);
+        if (stage_lock == true)
+        {
+            SoundManager.GetInst().PlayClickSound();
+            SceneManager.LoadScene(nextscene_num);
+        }
     }
 }
