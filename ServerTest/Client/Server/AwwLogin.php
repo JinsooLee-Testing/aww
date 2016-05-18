450e689d74fb599054463c46fa5d13da2266b9d4
lee Jinsoo <tmf2676@gmail.com>
2016-05-18 20:14:49 +0900

    <php - unity 연동 //  데이터 json 형식으로 보낼수 있도록 php수정  작업중 >

--- ServerTest/Client/Server/AwwLogin.php
+++ ServerTest/Client/Server/AwwLogin.php
@@ -1,23 +1,25 @@
 <?php
    $ID = $_REQUEST["ID"];
    $PASSWD = $_REQUEST["PASSWD"];
-   $conn = mysql_connect("localhost","root","111111") or die("connect_fail.");
+   $conn = mysql_connect("localhost","root","111111") or die("접속에 실패했습니다.");
   $c1_d = $_POST['ID'];
   $c2_d = $_POST['PASSWD'];
   mysql_select_db("test_login");
-
   $query = mysql_query("SELECT * FROM login_server WHERE ID LIKE '%$c1_d%'");
 
-  $result = mysql_fetch_array($query);
-  if(($_POST['ID'] === $result[0]) && ($_POST['PASSWD'] === $result[1]))
-  { 
-    print $result[2];
-  } 
-  else
+  $query = mysql_query("SELECT ID,PASSWD,STAGE FROM login_server");
+  
+  $rows = array();
+  while($r = mysql_fetch_assoc($query))
   {
-    echo "connect fail";
+    $rows[] = $r;
   }
-
+  print json_encode($rows);
+  /*$result = mysql_fetch_array($query);
+  if(($_POST['ID'] === $result[0]) && ($_POST['PASSWD'] === $result[1]) )
+  { 
+    echo "connect";
+  } 
   /*else  if(($_POST['ID'] === $result[0]) && ($_POST['PASSWD'] === $result[1]) && ($result[2] === "1"))
   { 
     echo "1";
@@ -29,19 +31,8 @@
     else  if(($_POST['ID'] === $result[0]) && ($_POST['PASSWD'] === $result[1]) && ($result[2] === "3"))
   { 
     echo "3";
-  } 
-    else  if(($_POST['ID'] === $result[0]) && ($_POST['PASSWD'] === $result[1]) && ($result[2] === "4"))
-  { 
-    echo "4";
-  } 
-    else  if(($_POST['ID'] === $result[0]) && ($_POST['PASSWD'] === $result[1]) && ($result[2] === "5"))
-  { 
-    echo "5";
-  } 
-  else
-  {
-    echo"잘못 보냈음";
-  }*/
+  } */
+
 
   
    mysql_close($conn);
