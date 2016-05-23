8d4cc7216dfe43424a6a1a475ab667ac864efe63
lee Jinsoo <tmf2676@gmail.com>
2016-05-23 20:03:59 +0900

    <php <-> Unitytest  // xml parse>

--- ServerTest/login_test/SelectCard.php
+++ ServerTest/login_test/SelectCard.php
@@ -1,22 +1,31 @@
-<?
-    $ID = $_REQUEST["ID"];
+﻿<?
+    //$ID = $_REQUEST["ID"];
     $conn = mysql_connect("localhost","root","111111") or die("접속에 실패했습니다.");
-    $c1_d = $_POST['ID'];
     mysql_select_db("test_login");
+    $query = mysql_query("SELECT * FROM skill_card");
 
-    //$query = mysql_query("SELECT * FROM login_server WHERE ID LIKE '%$c1_d%'");
-    $query = mysql_query("SELECT NAME FROM skill_card WHERE User LIKE  '%$c1_d%'");
-    //$result = mysql_fetch_array($query);
-    //$result = mysql_fetch_array($query);
+    if(!$query)
+    	id("DB access failed : " .mysql_error());
 
-    while($result = mysql_fetch_row($query)){
-  	print $result[0];
-  	print "\n" ;
-  	//print $result[1];
-  	//print " : ";
-  	//print $result[2];
-  }
+    $num_result = mysql_num_rows($query);
 
-     //$result = mysql_fetch_array($query);
-     
+    header ("Contect-type: text/xml");
+    echo"<xml version=\"1.0\" encoding=\"utf-8\" ?> \n";
+
+    echo "<record>";
+    for($i=0; $i<$num_result; ++$i)
+    {
+      $row = mysql_fetch_array($query);
+
+      echo "<item> ";
+      echo "SKILLNAME='$row[SKILLNAME]' ";
+      echo "COST='$row[COST]' ";
+      echo "TYPE='$row[TYPE]' ";
+      echo "DAMAGE='$row[DAMAGE]' ";
+      echo "RANGEVIEW='$row[RANGEVIEW]' ";
+      echo "RANGEOFACT='$row[RANGEOFACT]' ";
+      echo ."</item>";
+    }
+    echo"</record>";
+    mysql_close($conn);
 ?>
