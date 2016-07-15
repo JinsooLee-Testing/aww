91da5d0a947861bd9bea5dc3d84dabee6fff7b0f
lee Jinsoo <tmf2676@gmail.com>
2016-07-15 23:29:09 +0900

    깃 용량 최적화1

--- ServerTest/11.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_GET['id'] === 'lee'){
-    echo 'right';
-} else {
-   echo 'wrong';
-}
-
-?>
--- ServerTest/prac3/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-   $a = $_REQUEST["a"];
-   $b = $_REQUEST["b"];
-   echo $a+$b;
-   ?>
