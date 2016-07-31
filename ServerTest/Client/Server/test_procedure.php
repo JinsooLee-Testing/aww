6ab65166e8a89544c969120e74c906a2dd423dce
lee Jinsoo <tmf2676@gmail.com>
2016-07-31 23:26:24 +0900

    스토어드 프로시저 연습 구현

--- ServerTest/Client/Server/test_procedure.php
+++ ServerTest/Client/Server/test_procedure.php
@@ -1,15 +1,14 @@
 <?   
    $nam = "lee";
-   $adds = "dangsoodong";
 
    $mysqli = new mysqli('localhost','root','111111','just_test');
    if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-   if(!$mysqli->query("CALL p(1)")){
+   /*if(!$mysqli->query("CALL p(1)")){
     echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-      if(!$mysqli->query("CALL MY_PRO($nam,$adds)")){
+}*/
+      if(!$mysqli->query("CALL MY_PRO('$nam',13)")){
     echo "CALL failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
 ?>
