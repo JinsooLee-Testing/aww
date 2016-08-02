a3f508eb938000ff74e5805c97930e47ffe32e29
lee Jinsoo <tmf2676@gmail.com>
2016-08-02 19:53:20 +0900

    서버 연동 부분 수정 // 잡다한것들 제거

---
+++ ServerTest/Client/srpg/Assets/Scripts/Server/LoginManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class LoginManager : MonoBehaviour
+{
+    InputField id;
+    InputField password;
+    // Use this for initialization
+    public void SetID(InputField id)
+    {
+        this.id = id;
+    }
+    
+    public void SetPassword(InputField password)
+    {
+        this.password = password;
+    }
+
+    public void LoginPhp()
+    {
+        StartCoroutine(Printphp());
+    }
+
+    IEnumerator Printphp()
+    {
+		string url = "http://localhost/Aww/AwwLogin.php";
+        WWWForm form = new WWWForm();
+        form.AddField("ID", id.text);
+        form.AddField("PASSWD", password.text);
+
+        WWW www = new WWW(url, form);
+
+        yield return www;
+
+
+        if (null == www.error)
+        {
+            //Debug.Log(www.text);
+            Application.LoadLevel("MainTitle");
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+
+    }
+
+
+}
