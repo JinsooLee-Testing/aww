9c55f1f2cccd21b5bad4f7b0c3e3409b9ff4bb80
lee Jinsoo <tmf2676@gmail.com>
2016-08-15 01:33:19 +0900

    Send(Json) Recv(XML) 구현중 php로 정상적으로 전송 성공

--- ServerTest/login_test/unity/Assets/Resources/Script/CardBase.cs
+++ ServerTest/login_test/unity/Assets/Resources/Script/CardBase.cs
@@ -4,26 +4,11 @@ using System.Collections;
 public class CardBase : MonoBehaviour {
 
 
-    string mSkillName;
-    CardBase inst = null;
+    public string mCardName;
 
-    public CardBase GetInst()
-    {
-       if (!inst)
-            Debug.Log("inst is not getting");
-        return inst;
-    }
-	// Use this for initialization
-	void Start () {
-        inst = this;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    void Start()
+    { }
+    void Update()
+    { }
 
-    public void SetCardName(string skilName)
-    {
-        this.mSkillName = mSkillName;
-    }
 }
--- ServerTest/login_test/unity/Assets/Resources/Script/ObjectManager.cs
+++ ServerTest/login_test/unity/Assets/Resources/Script/ObjectManager.cs
@@ -4,16 +4,41 @@ using System.Collections.Generic;
 using System.Xml;
 public class ObjectManager : MonoBehaviour
 {
-    //CardBase cb = null;
-    void Start()
+    private static ObjectManager inst = null;
+    public static ObjectManager GetInst()
     {
+        return inst;
+    }
+    //CardBase cb = null;
+    void Awake()
+    {
+        inst = this;
         StartCoroutine(LoadCard());
         //cb = new CardBase();
     }
 
-    
-
-    IEnumerator LoadCard()
+    public void startSaveCoroutine(string str)
+    {
+        StartCoroutine(SaveCard(str));
+    }
+    IEnumerator SaveCard(string str) // 카드 정보를 php 로 전송 
+    {
+      
+        string url = "http://localhost/Aww/AwwCardDeckSave.php";
+       WWWForm form = new WWWForm();
+         form.AddField("CARD",str);
+        
+        WWW www = new WWW(url,form);
+        yield return www;
+        if (null == www.error)
+        {
+            //Debug.Log("not error");
+            Debug.Log(www.text);
+        }
+        else
+            Debug.Log(www.error);
+    }
+    IEnumerator LoadCard() // 카드 정보를 php로부터  받아옴 
     {
         string url = "http://localhost/Aww/AwwSelectObjectCard.php";
 
@@ -26,17 +51,18 @@ public class ObjectManager : MonoBehaviour
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.LoadXml(www.text.Trim());
 
-           /* foreach (XmlNode node in xmlDoc.SelectNodes("XML/DATA"))
+           foreach (XmlNode node in xmlDoc.SelectNodes("XML/DATA"))
             {
-                Debug.Log("NAME: " + node.SelectSingleNode("NAME").InnerText);
-                Debug.Log("ATT: " + node.SelectSingleNode("ATT").InnerText);
+                //Debug.Log("NAME: " + node.SelectSingleNode("NAME").InnerText);
+
+               /* Debug.Log("ATT: " + node.SelectSingleNode("ATT").InnerText);
                 Debug.Log("HP: " + node.SelectSingleNode("HP").InnerText);
                 Debug.Log("MOVE: " + node.SelectSingleNode("MOVE").InnerText);
                 Debug.Log("VIEW: " + node.SelectSingleNode("VIEW").InnerText);
                 Debug.Log("SIZE: " + node.SelectSingleNode("SIZE").InnerText);
                 Debug.Log("ATTRIBUTE: " + node.SelectSingleNode("ATTRIBUTE").InnerText);
-                Debug.Log("PLAYERSELECT: " + node.SelectSingleNode("PLAYERSELECT").InnerText);
-            }*/
+                Debug.Log("PLAYERSELECT: " + node.SelectSingleNode("PLAYERSELECT").InnerText);*/
+            }
         }
         else
             Debug.Log("Load Fail");
