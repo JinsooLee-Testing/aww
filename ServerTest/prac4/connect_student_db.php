1c6ccd31a99740829d474aee58368098c3e91c89
lee Jinsoo <tmf2676@gmail.com>
2016-08-15 03:37:29 +0900

    Unity php mysql 연동 성공

--- ServerTest/prac4/connect_student_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $connect=@mysql_connect("localhost","root","111111") or die(mysql_error());
-  mysql_select_db("studentdb",$connect) or die(mysql_error());
-   echo"db connect complete");
- ?>
